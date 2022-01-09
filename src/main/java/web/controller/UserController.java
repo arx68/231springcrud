47db0edc9f3488f76e711cca0f0a1544c9088c33
arx68 <arh1744@yandex.ru>
2022-01-10 04:10:30 +0500

    add maven.compiler.source 1.7
    add maven.compiler.source 1.7

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +11,12 @@ import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/user", method=RequestMethod.PUT)
+@RequestMapping(value = "/user")
 public class UserController {
 
     private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
